bf53a7ff18fd29aec4620636f36421ba3f315582
Sourabh Ukkalgaonkar <sourabh.ukkalgaonkar@gmail.com>
2024-02-17 13:44:21 +0530

    Added user login feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
+# frozen_string_literal: true
+
+# module to create some helper method used in the whole project.
 module ApplicationHelper
+  def flash_class(obj)
+    case obj
+    when 'alert'
+      'alert alert-info'
+    when 'error'
+      'alert alert-danger'
+    else
+      'alert alert-success'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :events
+
+  root 'events#index', only: %i[index create]
 end
